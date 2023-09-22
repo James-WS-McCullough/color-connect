dd523270ae9880584359fc11625f3a8c85002fec
James McCullough <james.mccullough@aaoglobal.com>
2023-09-22 09:04:56 +0100

    grid tidying

--- src/components/GridBox.tsx
+++ src/components/GridBox.tsx
@@ -1,13 +1,15 @@
 import { Box, HStack, VStack } from "@chakra-ui/react";
 import Circle from "./Circle";
+import { GridBoxPath } from "../types";
 
 type GridBoxProps = {
   color?: string;
   x: number;
   y: number;
+  path: GridBoxPath;
 };
 
-const GridBox: React.FC<GridBoxProps> = ({ color, x, y }) => {
+const GridBox: React.FC<GridBoxProps> = ({ color, x, y, path }) => {
   return (
     <Box
       w="80px"
@@ -20,13 +22,39 @@ const GridBox: React.FC<GridBoxProps> = ({ color, x, y }) => {
       {color && <Circle color={color} />}
 
       <VStack spacing="0" width="100%" height="100%">
-        <Box w="20%" h="40%" backgroundColor="red" />
+        <Box
+          w="20%"
+          h="40%"
+          backgroundColor={color}
+          opacity={path.up ? 1 : 0}
+        />
         <HStack spacing="0" height="20%" width="100%">
-          <Box w="40%" h="100%" backgroundColor="red" />
-          <Box w="20%" h="100%" backgroundColor="red" />
-          <Box w="40%" h="100%" backgroundColor="red" />
+          <Box
+            w="40%"
+            h="100%"
+            backgroundColor="red"
+            opacity={path.left ? 1 : 0}
+          />
+
+          <Box
+            w="20%"
+            h="100%"
+            backgroundColor="red"
+            opacity={path.left || path.right || path.up || path.down ? 1 : 0}
+          />
+          <Box
+            w="40%"
+            h="100%"
+            backgroundColor="red"
+            opacity={path.right ? 1 : 0}
+          />
         </HStack>
-        <Box w="20%" h="40%" backgroundColor="red" />
+        <Box
+          w="20%"
+          h="40%"
+          backgroundColor="red"
+          opacity={path.down ? 1 : 0}
+        />
       </VStack>
     </Box>
   );
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type GridBoxPath = {
+  up: boolean;
+  down: boolean;
+  left: boolean;
+  right: boolean;
+};
