5c5d2f916d9c7e29100dbd6a6d3786962ad28a62
James McCullough <james.mccullough@aaoglobal.com>
2024-04-05 11:20:08 +0100

    don't play sounds on mobile

--- src/utils/playSFX.ts
+++ src/utils/playSFX.ts
@@ -1,4 +1,7 @@
 export const playSFX = (filename: string) => {
+  // if on mobile, don't play sound
+  if (window.innerWidth < 768) return;
+
   const success = new Audio(filename);
   success.volume = 0.5;
   success.play();
